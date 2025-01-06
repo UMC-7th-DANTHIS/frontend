548ecc59b3db86b28fd7e4f82196fb28e0d33457
mlnls <tmdals140@naver.com>
2025-01-06 15:47:30 +0900

    feat: 기초 구조 제작

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
-import './App.css';
-import MainPage from "./pages/MainPage" 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import MainLayout from "./layout/MainLayout";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-                     
-    </Routes>
-  </Router>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/" element={<MainPage />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Topbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const Topbar = () => {
+  return (
+    <>
+      <Container></Container>
+    </>
+  );
+};
+
+export default Topbar;
+
+const Container = styled.div`
+  background-color: black;
+  width: 100%;
+  height: 221px;
+`;
