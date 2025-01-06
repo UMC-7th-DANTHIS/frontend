38ea24e707cee617eeb72e8b2bacc1385447488e
chasyuss <cotn963@gmail.com>
2025-01-06 21:09:02 +0900

    feat: 마이페이지 사이드바 및 페이지 렌더링 초기

---
+++ src/pages/mypage/components/MyLikeClass.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyLikeClass = () => {
+    return (
+        <div>MyLikeClass</div>
+    )
+}
+
+export default MyLikeClass
---
+++ src/pages/mypage/components/MyRegisterClass.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyRegisterClass = () => {
+    return (
+        <div>MyRegisterClass</div>
+    )
+}
+
+export default MyRegisterClass
