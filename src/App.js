683b13a158effd368843ef8000393ff514388286
chasyuss <cotn963@gmail.com>
2025-01-06 21:09:02 +0900

    feat: 마이페이지 사이드바 및 페이지 렌더링 초기

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import MainLayout from "./layout/MainLayout";
 import MainPage from "./pages/MainPage";
+import MypageLayout from "./pages/mypage/MypageLayout";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route path="/" element={<MainPage />} />
+          <Route path="/mypage" element={<MypageLayout />}></Route>
         </Route>
       </Routes>
     </Router>
---
+++ src/pages/mypage/components/MyLikeDancer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MyLikeDancer = () => {
+    return (
+        <div>MyLikeDancer</div>
+    )
+}
+
+export default MyLikeDancer
