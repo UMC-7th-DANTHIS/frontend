ced37eb44ff270c476cc336f6438fb908dd22ac6
mlnls <tmdals140@naver.com>
2025-01-07 23:46:57 +0900

    feat: Dummy Data 제작해두기 및 페이지 라우팅 설정
    feat: #7

---
+++ src/pages/CommunityPost.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+import dummyContent from "../store/community/dummyContent";
+
+const CommunityPost = () => {
+  return <div></div>;
+};
+
+export default CommunityPost;
