7366484c47759aa5c640287d1c18e77993bae079
Sowon Kim <sso626262@naver.com>
2025-01-09 21:16:57 +0900

    feat: 수업 등록 페이지 기본 구조 생성 (#11)

---
+++ src/pages/add-new-class/ClassRegistration.jsx
@@ -0,0 +1,48 @@
+import react, { useState } from "react";
+import styled from "styled-components";
+import NewClassForm from "./NewClassForm";
+import RegistrationComplete from "./ResgitrationComplete";
+
+const ClassRegistration = () => {
+  const [isRegistered, setIsRegistered] = useState(false);
+
+  const handleRegister = () => {
+    // 수업 등록 처리
+    setIsRegistered(true);
+  };
+
+  return (
+    <Container>
+      <Registration>
+        <Title>댄스 수업 등록</Title>
+        {!isRegistered ? (
+          <NewClassForm onRegister={handleRegister} />
+        ) : (
+          <RegistrationComplete />
+        )}
+      </Registration>
+    </Container>
+  );
+};
+
+export default ClassRegistration;
+
+const Container = styled.div`
+  display: flex;
+  background-color: black;
+  justify-content: center;
+`;
+const Registration = styled.div`
+  width: 1440px;
+  justify-items: center;
+`;
+const Title = styled.div`
+  color: var(--main_white, #fff);
+  font-family: Pretendard;
+  font-size: 48px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-top: 33px;
+  margin-bottom: 70px;
+`;
---
+++ src/pages/add-new-class/ResgitrationComplete.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import styled from "styled-components";
+import Check from "../../assets/check.svg";
+import { useNavigate } from "react-router-dom";
+
+const RegistrationComplete = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Container>
+      <Ellipse>
+        <img src={Check} alt="Check Icon" />
+      </Ellipse>
+      <Complete>댄스 수업 등록 신청이 완료되었어요!</Complete>
+      <Detail>
+        운영진의 검토 이후, 정식 댄스 수업으로 등록될 수 있어요. {"\n"}
+        등록 신청에 감사드려요 :)
+      </Detail>
+      <BackToHomeBtn
+        type="button"
+        onClick={() =>
+          navigate("/", {
+            replace: false,
+            state: {},
+          })
+        }
+      >
+        <BackToHomeBtnText>홈 화면으로 돌아가기</BackToHomeBtnText>
+      </BackToHomeBtn>
+    </Container>
+  );
+};
+
+export default RegistrationComplete;
+
+const Container = styled.div`
+  justify-items: center;
+  padding-bottom: 181.61px;
+`;
+const Ellipse = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 320px;
+  height: 320px;
+  flex-shrink: 0;
+  border-radius: 320px;
+  background: var(
+    --main-gradation,
+    linear-gradient(90deg, #b30505 0%, #9819c3 100%)
+  );
+`;
+const Complete = styled.div`
+  color: var(--main_white, #fff);
+  font-family: Pretendard;
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-top: 70.96px;
+  margin-bottom: 40px;
+`;
+const Detail = styled.div`
+  color: var(--text_secondary-gray, #b2b2b2);
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  white-space: pre-line;
+`;
+const BackToHomeBtn = styled.button`
+  width: 300px;
+  height: 52px;
+  flex-shrink: 0;
+  border: none;
+  border-radius: 15px;
+  background: var(--main_purple, #9819c3);
+  margin-top: 59.51px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+const BackToHomeBtnText = styled.div`
+  color: var(--main_white, #fff);
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
