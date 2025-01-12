a8ba8bc755e1ece5bc227fa371bd82bbc8f7f2a3
mlnls <tmdals140@naver.com>
2025-01-12 16:57:21 +0900

    feat: 검색창 및 해시태그 구현
    feat: #18

--- src/components/Search/SearchBar.jsx
+++ src/components/Search/SearchBar.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import Searchicon from "../../assets/searchicon.svg";
 
 const SearchBar = () => {
   return (
     <Container>
       <InputContainer>
         <Input defaultValue="Tania" />
+        <SearchButton>
+          <SearchIcon src={Searchicon} alt="search" />
+        </SearchButton>
       </InputContainer>
+      <HashTagContainer>
+        <UpperContainer>
+          <UpperHash>#스근한</UpperHash>
+          <UpperHash>#에너제틱</UpperHash>
+          <UpperHash>#섹시</UpperHash>
+          <UpperHash>#맨발</UpperHash>
+          <UpperHash>#유산소</UpperHash>
+        </UpperContainer>
+        <DownContainer>
+          <DownHash>#무거운</DownHash>
+          <DownHash>#아프로</DownHash>
+          <DownHash>#강렬한</DownHash>
+          <DownHash>#재즈/컨템포러리</DownHash>
+          <DownHash>#감성적인</DownHash>
+        </DownContainer>
+      </HashTagContainer>
     </Container>
   );
 };
@@ -18,12 +38,14 @@ const Container = styled.div`
 `;
 
 const InputContainer = styled.div`
+  display: flex;
+
   margin-left: 387px;
   margin-right: 351px;
   padding-left: 42px;
   padding-top: 19px;
 
-  width: 660px;
+  width: 640px;
   height: 57px;
 
   border-radius: 90px;
@@ -37,6 +59,7 @@ const Input = styled.input`
   outline: none;
 
   height: 40px;
+  width: 530px;
 
   color: white;
   font-size: 33.524px;
@@ -44,5 +67,76 @@ const Input = styled.input`
   font-weight: 400;
   line-height: normal;
 `;
+
+const SearchButton = styled.button`
+  background: none;
+  border: none;
+  display: flex;
+
+  margin-left: 30px;
+`;
+
+const SearchIcon = styled.img`
+  width: 33px;
+  height: 33px;
+  flex-shrink: 0;
+`;
+
+const HashTagContainer = styled.div`
+  margin-left: 303px;
+  margin-right: 303px;
+  margin-top: 35px;
+  margin-bottom: 76px;
+
+  width: 833px;
+  height: 124px;
+`;
+
+const UpperContainer = styled.div`
+  display: flex;
+  gap: 46px;
+`;
+
+const UpperHash = styled.div`
+  padding: 0px 34.5px;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 80px;
+  border: 2px solid #bf00ff;
+
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 50px;
+  letter-spacing: -1px;
+
+  color: #b2b2b2;
+`;
+
+const DownContainer = styled.div`
+  display: flex;
+  gap: 15px;
+  margin-top: 24px;
+`;
+
+const DownHash = styled.div`
+  padding: 0px 36.7px;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 80px;
+  border: 2px solid #bf00ff;
+
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 50px;
+  letter-spacing: -1px;
+
+  color: #b2b2b2;
+`;
+
+const SelectContainer = styled.div``;
 
 export default SearchBar;
