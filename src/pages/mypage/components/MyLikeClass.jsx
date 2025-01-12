7f9bc62a9fc84a0ec6148b02b04e27a35335d823
chasyuss <cotn963@gmail.com>
2025-01-12 12:36:33 +0900

    chore: sample image 임시 적용

--- src/pages/mypage/components/MyLikeClass.jsx
+++ src/pages/mypage/components/MyLikeClass.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import sampleImage from '../../../assets/image.png'
 
 const MyLikeClass = () => {
     return (
         <ClassContainer>
             {Array.from({ length: 6 }).map((_, index) => (
                 <ClassList key={index}>
-                    <div>내용</div>
+                    <Image src={sampleImage} alt={`Class ${index + 1}`} />
                 </ClassList>
             ))}
         </ClassContainer>
@@ -32,4 +33,11 @@ const ClassList = styled.div`
     height: 280px;
     border-radius: 10px;
     background-color: #333;
+`;
+
+const Image = styled.img`
+    width: 280px;
+    height: 280px;
+    object-fit: cover;
+    border-radius: 10px;
 `;
