81c47052df189c05f2c6e1603ce48c97ea9019fe
mlnls <tmdals140@naver.com>
2025-01-13 16:30:53 +0900

    feat: 검색 Dancer 제작 완료
    feat: #18

---
+++ src/store/dummyDancer.js
@@ -0,0 +1,14 @@
+const dummyDancer = [
+  {
+    Dancer: 'Tania',
+    Instagram: 'Tania_Dancer',
+    Genre: '걸스힙합, 코레오'
+  },
+  {
+    Dancer: 'mlnls',
+    Instagram: 'mlnls',
+    Genre: '개쩌는춤, 배우러오던가ㅋ'
+  }
+];
+
+export default dummyDancer;
