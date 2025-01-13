9ef19710a093263238f6f98d823c2fb24098fbc5
mlnls <tmdals140@naver.com>
2025-01-14 04:57:52 +0900

    feat: 유행하는 수업 컴포넌트 기틀 제작
    feat: #32

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 import HotDancer from '../components/Home/HotDancer';
+import HotClass from '../components/Home/HotClass';
 
 const HomePage = () => {
   return (
     <Container>
       <HotDancer />
+      <HotClass />
     </Container>
   );
 };
