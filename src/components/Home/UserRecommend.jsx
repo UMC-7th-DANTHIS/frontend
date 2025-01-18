ee2631fcf2c498055a4fd6ec7496787c39b907fd
mlnls <tmdals140@naver.com>
2025-01-18 19:13:11 +0900

    feat: dummyData 연동 성공 및 메인 페이지 기본 UI 제작 마무리
    feat: #32

--- src/components/Home/UserRecommend.jsx
+++ src/components/Home/UserRecommend.jsx
@@ -2,8 +2,6 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const UserRecommend = ({ dummyUserDancer, dummyUserClass }) => {
-  console.log(dummyUserDancer);
-
   return (
     <Container>
       <HeadContainer>
@@ -25,6 +23,24 @@ const UserRecommend = ({ dummyUserDancer, dummyUserClass }) => {
           </DancerContent>
         ))}
       </DancerContainer>
+      <Header>오로지 00 님을 위한 맞춤형 수업이에요</Header>
+      <ClassContainer>
+        {dummyUserClass?.map((Class) => (
+          <ClassContent>
+            <ClassImage src={Class.Image} alt={'프로필 이미지'} />
+            <TextContainer>
+              <ClassName>{Class.Title}</ClassName>
+              <ClassDancer>{Class.Dancer}</ClassDancer>
+              <ClassDancer>{Class.Genre}</ClassDancer>
+              <ClassHashContainer>
+                {Class?.Hashtag.map((Hashtag) => (
+                  <ClassHashtag># {Hashtag}</ClassHashtag>
+                ))}
+              </ClassHashContainer>
+            </TextContainer>
+          </ClassContent>
+        ))}
+      </ClassContainer>
     </Container>
   );
 };
@@ -34,6 +50,8 @@ export default UserRecommend;
 const Container = styled.div`
   margin-left: 100px;
   margin-right: 100px;
+
+  padding-bottom: 60px;
 `;
 
 const HeadContainer = styled.div`
@@ -184,3 +202,58 @@ const DancerGenre = styled.div`
 `;
 
 const ClassContainer = styled.div``;
+
+const ClassContent = styled.div`
+  display: inline-flex;
+  flex-direction: row;
+  width: 620px;
+  height: 200px;
+
+  margin-bottom: 113px;
+`;
+
+const ClassImage = styled.img`
+  display: inline-block;
+  width: 200px;
+  height: 200px;
+
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.25);
+`;
+
+const TextContainer = styled.div`
+  display: inline-block;
+  flex-direction: column;
+  padding-left: 30px;
+  align-content: center;
+
+  height: 200px;
+`;
+
+const ClassName = styled.div`
+  color: #fff;
+  font-size: 28px;
+  font-weight: 600;
+  margin-bottom: 15px;
+`;
+
+const ClassDancer = styled.div`
+  color: #b2b2b2;
+  font-size: 22px;
+  font-weight: 600;
+  line-height: 28px;
+`;
+
+const ClassHashContainer = styled.div`
+  padding-top: 5px;
+
+  color: #bf00ff;
+  font-size: 22px;
+  font-weight: 500;
+  line-height: 32px;
+`;
+
+const ClassHashtag = styled.div`
+  display: inline-block;
+  padding-right: 10px;
+`;
