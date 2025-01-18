75b43de95004249bba543d30ca91159a146bf642
mlnls <tmdals140@naver.com>
2025-01-18 17:19:24 +0900

    feat: dummyUserClass 추가
    feat: #32

---
+++ src/store/main/dummyUserClass.js
@@ -0,0 +1,32 @@
+const dummyUserClass = [
+  {
+    Title: 'EZ-Master class',
+    Dancer: 'Funky Y',
+    Genre: '왁킹',
+    Image: 'https://placehold.jp/200x200.png',
+    Hashtag: ['에너제틱', '섹시', '유산소']
+  },
+  {
+    Title: 'Worst behavior',
+    Dancer: 'Groot',
+    Genre: '코레오',
+    Image: 'https://placehold.jp/200x200.png',
+    Hashtag: ['에너제틱', '강렬한', '유산소']
+  },
+  {
+    Title: 'LEGO Hiphop class',
+    Dancer: 'LEGO',
+    Genre: '힙합',
+    Image: 'https://placehold.jp/200x200.png',
+    Hashtag: ['스근한']
+  },
+  {
+    Title: 'APT.- Learner class',
+    Dancer: 'Funky Y',
+    Genre: '코레오',
+    Image: 'https://placehold.jp/200x200.png',
+    Hashtag: ['스근한']
+  }
+];
+
+export default dummyUserClass;
