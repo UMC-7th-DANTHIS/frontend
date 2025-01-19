a5437de73d745e4e31b561f831c531cbc3bdc7fd
mlnls <tmdals140@naver.com>
2025-01-19 17:40:23 +0900

    chore: Pagination 사용법 주석 추가
    chore: #37

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+// Pagination 컴포넌트 사용법
+// dataLength: page를 나타내고 싶은 데이터들의 length 를 넣어주시면 됩니다 => ex. data.length
+// perData: 한 페이지에 몇 개의 데이터가 보여질 것인지를 넣어주시면 됩니다 => ex. 5
+// 그리고 pagination을 참조하려는 페이지에서
+// const [currentPage, setCurrentPage] = useState(1);
+// 이걸 처음에 선언해주시고 해당 페이지를 누르거나 넘기면 해당 번호로 설정되게끔 짜놨으니 그걸로 데이터 slice 해서 사용하면 될 것 같습니다.
+// 제가 실제로 적용까지 이어서 하려고 했는데 CSS는 다 각기 다른 방식으로 작업하셔서 잘 모르겠습니다 ㅜㅜ
+// 그쪽 부분만 진행해주셨으면 좋겠습니다 ㅎ.ㅎ
+
 const Pagination = ({ dataLength, perData, currentPage, setCurrentPage }) => {
   const [layer, setLayer] = useState(0);
 
--- src/pages/profile/DancerProfile.jsx
+++ src/pages/profile/DancerProfile.jsx
@@ -1,83 +1,77 @@
-import React, {useState} from 'react'
-import styled from 'styled-components'
-import Profile from './components/Profile'
-import IntroduceTab from './components/IntroduceTab'
-import ClassTab from './components/ClassTab'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Profile from './components/Profile';
+import IntroduceTab from './components/IntroduceTab';
+import ClassTab from './components/ClassTab';
 
 const DancerProfile = () => {
-  const [activeTab, setActiveTab] = useState("소개");
-
+  const [activeTab, setActiveTab] = useState('소개');
   return (
     <Layout>
       <Profile />
       <TabContainer>
-        <Tab active = {activeTab === "소개"} onClick={()=> setActiveTab("소개")}>
+        <Tab active={activeTab === '소개'} onClick={() => setActiveTab('소개')}>
           소개
         </Tab>
-        <Tab active = {activeTab === "등록된 수업"} onClick={()=> setActiveTab("등록된 수업")}>
+        <Tab
+          active={activeTab === '등록된 수업'}
+          onClick={() => setActiveTab('등록된 수업')}
+        >
           등록된 수업
         </Tab>
       </TabContainer>
       <ContentContainer>
-        {activeTab ==="소개" && <IntroduceTab />}
-        {activeTab ==="등록된 수업" && <ClassTab />}
-
+        {activeTab === '소개' && <IntroduceTab />}
+        {activeTab === '등록된 수업' && <ClassTab />}
       </ContentContainer>
-       
-  
-        
     </Layout>
-  )
-}
+  );
+};
 
 export default DancerProfile;
 
 const Layout = styled.div`
-background : black;
-display : flex;
-flex-direction : column;
-
-
-`
+  background: black;
+  display: flex;
+  flex-direction: column;
+`;
 
 const TabContainer = styled.div`
-width: 1240px;
-height: 86.005px;
-flex-shrink: 0;
-background : #9819C3;
-border-radius: 20px 20px 0px 0px;
-display : flex;
-align-items : center;
-margin-left : 100px;
-justify-content : center;
-
-`
+  width: 1240px;
+  height: 86.005px;
+  flex-shrink: 0;
+  background: #9819c3;
+  border-radius: 20px 20px 0px 0px;
+  display: flex;
+  align-items: center;
+  margin-left: 100px;
+  justify-content: center;
+`;
 
 const Tab = styled.div`
-display : flex;
-align-items : center;
-justify-content : center;
-width: 620px;
-height: 86.005px;
-flex-shrink: 0;
-color: var(--main_white, #FFF);
-text-align: center;
-font-family: Pretendard;
-font-size: 24px;
-font-style: normal;
-font-weight: 600;
-line-height: 50px; /* 208.333% */
-letter-spacing: -1.2px;
-cursor: pointer;
-border-radius: 20px 20px 0px 0px;
-border-top: 3px solid var(--main_purple, #9819C3);
-border-right: 3px solid var(--main_purple, #9819C3);
-border-left: 3px solid var(--main_purple, #9819C3);
-background: var(--main_black, #000);
-box-shadow: 0px 8px 16px 0px var(--main_purple, #9819C3) inset;
-background-color: ${(props) => (props.active ? "black" : "#9819C3")};
-transition: all 0.3s;
-` 
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 620px;
+  height: 86.005px;
+  flex-shrink: 0;
+  color: var(--main_white, #fff);
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 50px; /* 208.333% */
+  letter-spacing: -1.2px;
+  cursor: pointer;
+  border-radius: 20px 20px 0px 0px;
+  border-top: 3px solid var(--main_purple, #9819c3);
+  border-right: 3px solid var(--main_purple, #9819c3);
+  border-left: 3px solid var(--main_purple, #9819c3);
+  background: var(--main_black, #000);
+  box-shadow: 0px 8px 16px 0px var(--main_purple, #9819c3) inset;
+  background-color: ${(props) => (props.active ? 'black' : '#9819C3')};
+  transition: all 0.3s;
+`;
 
-const ContentContainer=styled.div`
-`
+const ContentContainer = styled.div``;
