cccd11713a3269106913c304de71e6e69f4b72b1
Sowon Kim <sso626262@naver.com>
2025-01-20 15:51:52 +0900

    chore: reviews 더미 데이터 초기화 수정 (#45)

--- src/pages/reservation/components/tabs/ReviewTab.jsx
+++ src/pages/reservation/components/tabs/ReviewTab.jsx
@@ -1,22 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Review from '../Review';
 import dummyReviews from '../../../../store/reservation/dummyReviews';
 import Pagination from '../Pagination';
 
 const ReviewTab = () => {
-  const [data, setData] = useState(dummyReviews);
+  const [reviews, setReviews] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const perData = 2; // 페이지 당 보여질 요소 개수
 
-  const numPerPage = 2; // 페이지 당 보여질 요소 개수
-  const totalPages = Math.ceil(data.length / numPerPage); // 총 페이지 개수
+  useEffect(() => {
+    setReviews(dummyReviews);
+  }, []);
+
+  const totalPages = Math.ceil(reviews.length / perData); // 총 페이지 개수
 
   // 현재 페이지에 보여질 요소 계산
   const getCurrentPageData = () => {
-    const startIndex = (currentPage - 1) * numPerPage;
-    const endIndex = startIndex + numPerPage;
+    const startIndex = (currentPage - 1) * perData;
+    const endIndex = startIndex + perData;
 
-    return dummyReviews.slice(startIndex, endIndex);
+    return reviews.slice(startIndex, endIndex);
   };
 
   // 페이지네이션 핸들러
