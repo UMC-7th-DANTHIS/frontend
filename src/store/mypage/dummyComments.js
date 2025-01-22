ac8a2370ff8b9895eaef5cd18daf7f84f5efa8e8
chasyuss <cotn963@gmail.com>
2025-01-22 15:09:24 +0900

    chore: 내가 쓴 게시물/댓글 더미데이터 적용

---
+++ src/store/mypage/dummyComments.js
@@ -0,0 +1,24 @@
+const dummyComments = [
+  {
+    id: 1,
+    postId: 100,
+    content: '보이넥스트도어의 ABCDLOVE 많관부'
+  },
+  {
+    id: 2,
+    postId: 101,
+    content: '보이넥스트도어의 오늘만 ILOVEYOU 많관부'
+  },
+  {
+    id: 3,
+    postId: 102,
+    content: '보이넥스트도어의 ABCDLOVE 많관부'
+  },
+  {
+    id: 4,
+    postId: 103,
+    content: '보이넥스트도어의 오늘만 ILOVEYOU 많관부'
+  }
+];
+
+export default dummyComments;
