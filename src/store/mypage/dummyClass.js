b4137bc34c8bc44c0eaf133b3a4f8a20aad8f12a
chasyuss <cotn963@gmail.com>
2025-01-22 15:09:43 +0900

    chore: 내가 찜한 수업 더미데이터 적용

---
+++ src/store/mypage/dummyClass.js
@@ -0,0 +1,40 @@
+const dummyClass = [
+  {
+    id: 1,
+    className: 'The Seed - Aurora',
+    dancerName: 'Parana',
+    images: [null, null, null]
+  },
+  {
+    id: 2,
+    className: 'Smoke - Dynamic Duo',
+    dancerName: 'Parana',
+    images: [null, null, null]
+  },
+  {
+    id: 3,
+    className: 'APT. - Learner class',
+    dancerName: 'Yechan',
+    images: [null, null, null]
+  },
+  {
+    id: 4,
+    className: 'EZ - Master class',
+    dancerName: 'Funky Y',
+    images: [null, null, null]
+  },
+  {
+    id: 5,
+    className: 'LEGO Hiphop class',
+    dancerName: 'Lego',
+    images: [null, null, null]
+  },
+  {
+    id: 6,
+    className: 'Sneakers - ITZY',
+    dancerName: 'Koi',
+    images: [null, null, null]
+  }
+];
+
+export default dummyClass;
