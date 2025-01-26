b983f19c737db722bda9c9bf32f3d7fbcb58e2a0
Sowon Kim <sso626262@naver.com>
2025-01-27 03:03:32 +0900

    chore: 탭 메뉴 크기 수정 (#45)

--- src/pages/reservation/components/TabMenu.jsx
+++ src/pages/reservation/components/TabMenu.jsx
@@ -44,7 +44,7 @@ const Container = styled.div`
   flex-direction: row;
   justify-content: center;
   width: 1240px;
-  height: 86.005px;
+  height: 86px;
   flex-shrink: 0;
   border-radius: 20px 20px 0px 0px;
   background: var(--main_purple, #9819c3);
@@ -54,7 +54,7 @@ const Tab = styled.div`
   justify-content: center;
   align-items: center;
   width: 410px;
-  height: 85px;
+  height: 83px;
   color: var(--main_white, #fff);
   text-align: center;
   font-family: Pretendard;
@@ -63,16 +63,14 @@ const Tab = styled.div`
   font-weight: 600;
   line-height: 50px; /* 178.571% */
   letter-spacing: -1.4px;
+  transition: all 0.3s;
+  border-radius: 20px 20px 0px 0px;
+  border-top: 3px solid var(--main_purple, #9819c3);
+  border-right: 3px solid var(--main_purple, #9819c3);
+  border-left: 3px solid var(--main_purple, #9819c3);
+  box-shadow: 0px 8px 16px 0px var(--main_purple, #9819c3) inset;
 
-  ${({ $isActive }) =>
-    $isActive &&
-    `
-    border-radius: 20px 20px 0px 0px;
-    border-top: 3px solid var(--main_purple, #9819c3);
-    border-right: 3px solid var(--main_purple, #9819c3);
-    border-left: 3px solid var(--main_purple, #9819c3);
-    background: var(--main_black, #000);
-    box-shadow: 0px 8px 16px 0px var(--main_purple, #9819c3) inset;`}
+  ${({ $isActive }) => $isActive && `background: var(--main_black, #000);`}
 
   &:hover {
     cursor: pointer;
