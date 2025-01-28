5fedbad8cc1db1e60f99d693c9eab8eb6b7fe59d
chasyuss <cotn963@gmail.com>
2025-01-28 19:14:21 +0900

    chore: 해당 classid에 맞게 navigate 되게 변경

---
+++ src/pages/mypage/components/review/ReviewDetail.jsx
@@ -0,0 +1,228 @@
+import React, { useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+import Alert from '../../../../components/Alert';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import dummyClass from '../../../../store/mypage/dummyClass';
+import MypageSidebar from '../../MypageSidebar';
+import ReviewForm from './ReviewForm';
+import PhotoUpload from './PhotoUpload';
+import StarRating from './StarRating';
+
+const ReviewDetail = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [starStates, setStarStates] = useState(Array(5).fill(false));
+  const [selectedImage, setSelectedImage] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
+  const [review, setReview] = useState("");
+  const [title, setTitle] = useState("");
+  const { id } = useParams();
+  const [classData, setClassData] = useState(null);
+  const selectedMenu = new URLSearchParams(location.search).get('menu') || 'myreview';
+  const handleMenuClick = (menuKey) => {
+    navigate(`/mypage?menu=${menuKey}`);
+  };
+
+  useEffect(() => {
+    const selectedClass = dummyClass.find((classItem) => classItem.id === parseInt(id));
+    setClassData(selectedClass);
+  }, [id]);
+
+  const handelReview = (e) => setReview(e.target.value);
+  const handleTitle = (e) => setTitle(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("제목", title);
+    console.log("리뷰", review);
+    console.log("별점", starStates.filter(state => state).length);
+    console.log("사진", selectedImage);
+    setTitle("");
+    setReview("");
+    setSelectedImage([]);
+  };
+
+  const toggleStar = (index) => {
+    setStarStates((prev) => prev.map((state, i) => (i === index ? !state : state)));
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) setSelectedImage(URL.createObjectURL(file));
+  };
+
+  const handleClickCancel = () => setShowAlert(true);
+  const hideClickCancel = () => setShowAlert(false);
+
+  return (
+    <>
+      <Container>
+        <MypageSidebar selectedMenu={selectedMenu} onMenuClick={handleMenuClick} />
+
+        {classData && (
+          <ReviewContainer>
+            <ClassTitle>{classData.className}</ClassTitle>
+            <Title>리뷰 작성</Title>
+            <Notice>
+              <li> * 제목은 최대 50자까지 입력 가능합니다. </li>
+              <li> * 내용은 최대 1000자까지 입력 가능합니다.</li>
+            </Notice>
+
+            <ReviewForm
+              title={title}
+              review={review}
+              handleTitle={handleTitle}
+              handelReview={handelReview}
+            />
+
+            <PhotoUpload selectedImage={selectedImage} setSelectedImage={setSelectedImage} />
+
+            {/* <PreviewImageContainer>
+              {selectedImage && (
+                <PreviewImage src={selectedImage} alt="selected image" />
+              )}
+            </PreviewImageContainer> */}
+
+            <StarRating starStates={starStates} toggleStar={toggleStar} />
+
+            <FinalSection>
+              <Buttons>
+                <CancelButton onClick={handleClickCancel}>
+                  취소
+                  {showAlert && (
+                    <Alert
+                      message={
+                        <span>
+                          <span>해당 페이지를 벗어나면 <br /></span>
+                          <span>작성 중인 글이 <ColoredText> 모두 삭제 </ColoredText> 됩니다 <br /></span>
+                          <span>떠나시겠습니까?</span>
+                        </span>
+                      }
+                      onClose={hideClickCancel}
+                      mariginsize="22px"
+                      ContainerWidth="280px"
+                      ContainerHeight="108px"
+                      marginsize="24px"
+                      AlertWidth="392px"
+                      AlertHeight="260px"
+                      showButtons={true}
+                      confirmLabel="남기"
+                      cancelLabel="떠나기"
+                    />
+                  )}
+                </CancelButton>
+
+                <SubmitButton onClick={handleSubmit}>작성</SubmitButton>
+              </Buttons>
+            </FinalSection>
+          </ReviewContainer>
+        )}
+      </Container>
+    </>
+  );
+}
+export default ReviewDetail;
+
+const Container = styled.div`
+    display: flex;
+`
+
+const ReviewContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: black;
+    color: #fff;
+    margin-top: 38px;
+    margin-left: 147px;
+`;
+
+const ClassTitle = styled.div`
+    color: #FFF;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    text-align: center;
+    margin-bottom: 23px;
+`
+
+const Title = styled.div`
+    font-weight: 600;
+    color: #FFF;
+    font-size: 22px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    margin-left: 55px;
+`;
+
+const Notice = styled.div`
+    color:#B2B2B2;
+    text-align: right;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+    list-style: none;
+    margin-bottom: 10px;
+`
+
+
+const PreviewImageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+const PreviewImage = styled.img`
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
+    border-radius: 10px;
+`;
+
+
+// 버튼 및 경고
+const FinalSection = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 244px;
+    gap: 24px;
+    margin-top: 21px;
+    justify-content: flex-end;
+`;
+
+const Buttons = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+`;
+
+const CancelButton = styled.button`
+    width: 64px;
+    height: 36px;
+    background-color: transparent;
+    color: #fff;
+    border: 2px solid #9819C3;
+    border-radius: 10px;
+    cursor: pointer;
+    font-weight: 600;
+
+    &:hover {
+        background-color: #9819C3;
+    }
+`;
+
+const SubmitButton = styled.button`
+    width: 64px;
+    height: 36px;
+    background-color: #9819C3;
+    color: #fff;
+    border: 2px solid #9819C3;
+    border-radius: 10px;
+    cursor: pointer;
+    font-weight: 600;
+`;
+
+//alert
+const ColoredText = styled.span`
+  color: #A60F62;
+`
