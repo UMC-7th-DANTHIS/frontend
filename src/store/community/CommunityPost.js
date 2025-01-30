e6acb7eb5e7fc390805837314c2aa1b1e6a6d70c
mlnls <tmdals140@naver.com>
2025-01-30 23:59:03 +0900

    chore: Community Page 더미 데이터 생성
    chore: #67

---
+++ src/store/community/CommunityPost.js
@@ -0,0 +1,182 @@
+const CommunityPost = [
+  {
+    id: 1,
+    user_id: 4,
+    title: 'Sample Title 1',
+    content: 'This is the content of post 1.',
+    image: 'https://example.com/image1.jpg',
+    views: 344,
+    created_at: '2025-01-29T14:55:06.671004'
+  },
+  {
+    id: 2,
+    user_id: 2,
+    title: 'Sample Title 2',
+    content: 'This is the content of post 2.',
+    image: 'https://example.com/image2.jpg',
+    views: 440,
+    created_at: '2024-12-26T14:55:06.671061'
+  },
+  {
+    id: 3,
+    user_id: 3,
+    title: 'Sample Title 3',
+    content: 'This is the content of post 3.',
+    image: 'https://example.com/image3.jpg',
+    views: 224,
+    created_at: '2025-01-13T14:55:06.671085'
+  },
+  {
+    id: 4,
+    user_id: 3,
+    title: 'Sample Title 4',
+    content: 'This is the content of post 4.',
+    image: 'https://example.com/image4.jpg',
+    views: 41,
+    created_at: '2024-11-28T14:55:06.671124'
+  },
+  {
+    id: 5,
+    user_id: 4,
+    title: 'Sample Title 5',
+    content: 'This is the content of post 5.',
+    image: 'https://example.com/image5.jpg',
+    views: 351,
+    created_at: '2024-10-23T14:55:06.671166'
+  },
+  {
+    id: 6,
+    user_id: 4,
+    title: 'Sample Title 6',
+    content: 'This is the content of post 6.',
+    image: 'https://example.com/image6.jpg',
+    views: 396,
+    created_at: '2024-11-05T14:55:06.671195'
+  },
+  {
+    id: 7,
+    user_id: 2,
+    title: 'Sample Title 7',
+    content: 'This is the content of post 7.',
+    image: 'https://example.com/image7.jpg',
+    views: 39,
+    created_at: '2024-11-19T14:55:06.671219'
+  },
+  {
+    id: 8,
+    user_id: 3,
+    title: 'Sample Title 8',
+    content: 'This is the content of post 8.',
+    image: 'https://example.com/image8.jpg',
+    views: 408,
+    created_at: '2025-01-05T14:55:06.671251'
+  },
+  {
+    id: 9,
+    user_id: 3,
+    title: 'Sample Title 9',
+    content: 'This is the content of post 9.',
+    image: 'https://example.com/image9.jpg',
+    views: 58,
+    created_at: '2025-01-05T14:55:06.671283'
+  },
+  {
+    id: 10,
+    user_id: 5,
+    title: 'Sample Title 10',
+    content: 'This is the content of post 10.',
+    image: 'https://example.com/image10.jpg',
+    views: 121,
+    created_at: '2024-12-07T14:55:06.671326'
+  },
+  {
+    id: 11,
+    user_id: 4,
+    title: 'Sample Title 11',
+    content: 'This is the content of post 11.',
+    image: 'https://example.com/image11.jpg',
+    views: 335,
+    created_at: '2024-11-08T14:55:06.671365'
+  },
+  {
+    id: 12,
+    user_id: 4,
+    title: 'Sample Title 12',
+    content: 'This is the content of post 12.',
+    image: 'https://example.com/image12.jpg',
+    views: 258,
+    created_at: '2025-01-14T14:55:06.671385'
+  },
+  {
+    id: 13,
+    user_id: 3,
+    title: 'Sample Title 13',
+    content: 'This is the content of post 13.',
+    image: 'https://example.com/image13.jpg',
+    views: 472,
+    created_at: '2025-01-09T14:55:06.671413'
+  },
+  {
+    id: 14,
+    user_id: 4,
+    title: 'Sample Title 14',
+    content: 'This is the content of post 14.',
+    image: 'https://example.com/image14.jpg',
+    views: 212,
+    created_at: '2024-12-18T14:55:06.671447'
+  },
+  {
+    id: 15,
+    user_id: 5,
+    title: 'Sample Title 15',
+    content: 'This is the content of post 15.',
+    image: 'https://example.com/image15.jpg',
+    views: 355,
+    created_at: '2024-12-22T14:55:06.671483'
+  },
+  {
+    id: 16,
+    user_id: 4,
+    title: 'Sample Title 16',
+    content: 'This is the content of post 16.',
+    image: 'https://example.com/image16.jpg',
+    views: 39,
+    created_at: '2024-12-24T14:55:06.671507'
+  },
+  {
+    id: 17,
+    user_id: 1,
+    title: 'Sample Title 17',
+    content: 'This is the content of post 17.',
+    image: 'https://example.com/image17.jpg',
+    views: 141,
+    created_at: '2024-11-15T14:55:06.671532'
+  },
+  {
+    id: 18,
+    user_id: 1,
+    title: 'Sample Title 18',
+    content: 'This is the content of post 18.',
+    image: 'https://example.com/image18.jpg',
+    views: 209,
+    created_at: '2024-11-24T14:55:06.671554'
+  },
+  {
+    id: 19,
+    user_id: 4,
+    title: 'Sample Title 19',
+    content: 'This is the content of post 19.',
+    image: 'https://example.com/image19.jpg',
+    views: 186,
+    created_at: '2024-11-15T14:55:06.671575'
+  },
+  {
+    id: 20,
+    user_id: 2,
+    title: 'Sample Title 20',
+    content: 'This is the content of post 20.',
+    image: 'https://example.com/image20.jpg',
+    views: 445,
+    created_at: '2024-12-23T14:55:06.671593'
+  }
+];
