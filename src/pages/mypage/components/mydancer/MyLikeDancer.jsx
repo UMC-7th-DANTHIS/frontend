a99467ade49acd7e189c5b685ccc274020699b41
chasyuss <cotn963@gmail.com>
2025-02-09 21:23:20 +0900

    feat: 내가 찜한 댄서 API 연결

--- src/pages/mypage/components/mydancer/MyLikeDancer.jsx
+++ src/pages/mypage/components/mydancer/MyLikeDancer.jsx
@@ -1,35 +1,81 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import sampleImage from '../../../../assets/image.png'
+import sampleImage from '../../../../assets/errorImage.svg'
 import Pagination from '../../../../components/Pagination';
-import dummyDancer from '../../../../store/mypage/dummyDancer';
+import api from '../../../../api/api';
+import LoadingSpinner from '../../../../components/LoadingSpinner';
 
 const MyLikeDancer = () => {
-  const data = dummyDancer;
+  const [dancers, setDancers] = useState([])
   const [currentPage, setCurrentPage] = useState(1);
   const perData = 6;
-  const filteredList = data.slice(
+  const filteredList = dancers.slice(
     perData * (currentPage - 1),
     perData * currentPage
   );
+  const [isLoading, setIsLoading] = useState(true);
+
+  //내가 찜한 댄서 api
+  const getDancer = async () => {
+    try {
+      const token = localStorage.getItem('token');
+
+      const response = await api.get('/users/dancers', {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      const dancerArray = response.data.data.dancers || [];
+      setDancers(dancerArray);
+      // console.log('dancers 상태:', response.data.data.dancers);
+    } catch (error) {
+      console.error('데이터 가져오기 실패', error.response?.data || error.message);
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      getDancer();
+    }, 1000);
+  }, []);
+
+  const handleImageError = (e) => {
+    e.target.src = sampleImage;
+  };
 
   return (
     <>
-      <DancerContainer>
-        {filteredList.map((data) => (
-          <DancerList key={data.id}>
-            <Image src={data.images[0] || sampleImage} alt={data.id} />
-            <Dancer> {data.dancerName} </Dancer>
-          </DancerList>
-        ))}
-      </DancerContainer>
-      <PaginationContainer>
-        <Pagination dataLength={data.length} perData={perData} currentPage={currentPage}
-          setCurrentPage={setCurrentPage} />
-      </PaginationContainer>
+      {isLoading ? (
+        <LoadingSpinner isLoading={isLoading} />
+      ) : (
+        <>
+          <DancerContainer>
+            {filteredList.map((dancer) => (
+              <DancerList key={dancer.id}>
+                <Image
+                  src={dancer.images[0] || dancer.images[1] || sampleImage}
+                  alt={dancer.dancerName}
+                  onError={handleImageError} // 이미지 로딩 실패시 임시 샘플 사진 출력 -> 추후 삭제 예정
+                />
+                <Dancer>{dancer.dancerName}</Dancer>
+              </DancerList>
+            ))}
+          </DancerContainer>
+          <PaginationContainer>
+            <Pagination
+              dataLength={dancers.length}
+              perData={perData}
+              currentPage={currentPage}
+              setCurrentPage={setCurrentPage}
+            />
+          </PaginationContainer>
+        </>
+      )}
     </>
   );
-}
+};
 
 export default MyLikeDancer;
 
