63edac2bbee88344339e3a268fa322070b19acd4
chasyuss <cotn963@gmail.com>
2025-02-10 15:17:18 +0900

    feat: api tanstack query로 변경

--- src/pages/mypage/components/mydancer/MyLikeDancer.jsx
+++ src/pages/mypage/components/mydancer/MyLikeDancer.jsx
@@ -1,78 +1,66 @@
-import React, { useEffect, useState } from 'react'
-import styled from 'styled-components'
-import sampleImage from '../../../../assets/errorImage.svg'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import sampleImage from '../../../../assets/errorImage.svg';
 import Pagination from '../../../../components/Pagination';
+import { useQuery } from '@tanstack/react-query';
 import api from '../../../../api/api';
 import LoadingSpinner from '../../../../components/LoadingSpinner';
 
 const MyLikeDancer = () => {
-  const [dancers, setDancers] = useState([])
   const [currentPage, setCurrentPage] = useState(1);
   const perData = 6;
-  const filteredList = dancers.slice(
-    perData * (currentPage - 1),
-    perData * currentPage
-  );
-  const [isLoading, setIsLoading] = useState(true);
 
-  //내가 찜한 댄서 api
-  const getDancer = async () => {
-    try {
-      const token = localStorage.getItem('token');
-
-      const response = await api.get('/users/dancers', {
-        headers: {
-          'Authorization': `Bearer ${token}`
-        }
-      });
-      const dancerArray = response.data.data.dancers || [];
-      setDancers(dancerArray);
-      // console.log('dancers 상태:', response.data.data.dancers);
-    } catch (error) {
-      console.error('데이터 가져오기 실패', error.response?.data || error.message);
-    } finally {
-      setIsLoading(false)
+  const { data, isLoading, isError, error } = useQuery(
+    {
+      queryKey: ['dancers'],
+      queryFn: async () => {
+        const token = localStorage.getItem('token');
+        const response = await api.get('/users/dancers', {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          },
+        });
+        return response.data.data.dancers || [];
+      },
     }
-  }
+  );
 
-  useEffect(() => {
-    setTimeout(() => {
-      getDancer();
-    }, 1000);
-  }, []);
+  const filteredList = data ? data.slice(perData * (currentPage - 1), perData * currentPage) : [];
 
   const handleImageError = (e) => {
     e.target.src = sampleImage;
   };
 
+  if (isLoading) {
+    return <LoadingSpinner isLoading={isLoading} />;
+  }
+
+  if (isError) {
+    return <div>Error: {error?.message}</div>;
+  }
+
   return (
     <>
-      {isLoading ? (
-        <LoadingSpinner isLoading={isLoading} />
-      ) : (
-        <>
-          <DancerContainer>
-            {filteredList.map((dancer) => (
-              <DancerList key={dancer.id}>
-                <Image
-                  src={dancer.images[0] || dancer.images[1] || sampleImage}
-                  alt={dancer.dancerName}
-                  onError={handleImageError} // 이미지 로딩 실패시 임시 샘플 사진 출력 -> 추후 삭제 예정
-                />
-                <Dancer>{dancer.dancerName}</Dancer>
-              </DancerList>
-            ))}
-          </DancerContainer>
-          <PaginationContainer>
-            <Pagination
-              dataLength={dancers.length}
-              perData={perData}
-              currentPage={currentPage}
-              setCurrentPage={setCurrentPage}
+      <DancerContainer>
+        {filteredList.map((dancer) => (
+          <DancerList key={dancer.id}>
+            <Image
+              src={dancer.images[0] || dancer.images[1]}
+              alt={dancer.dancerName}
+              onError={handleImageError} // 이미지 로딩 실패시 임시 샘플 사진 출력 -> 추후 삭제 예정
             />
-          </PaginationContainer>
-        </>
-      )}
+            <Dancer>{dancer.dancerName}</Dancer>
+          </DancerList>
+        ))}
+      </DancerContainer>
+      <PaginationContainer>
+        <Pagination
+          dataLength={data ? data.length : 0}
+          perData={perData}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+        />
+      </PaginationContainer>
     </>
   );
 };
@@ -86,7 +74,6 @@ const DancerContainer = styled.div`
   row-gap: 78px;
   margin-top: 40px;
   justify-content: center; 
-
 `;
 
 const DancerList = styled.div`
@@ -111,11 +98,13 @@ const Dancer = styled.div`
   line-height: normal;
   letter-spacing: -1.2px;
   margin-top: 5px;
-`
+`;
+
 const PaginationContainer = styled.div`
   margin-bottom: 246px;
   margin-top: 96px;
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
+
