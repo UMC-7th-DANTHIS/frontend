f5044548f193fa38b4996d98e3eb73375bb8b163
Sowon Kim <sso626262@naver.com>
2025-03-30 23:07:54 +0900

    refactor: ClassRegisterEdit TS 전환

---
+++ src/hooks/registration/usePut.ts
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import axiosInstance from '../../api/axios-instance';
+
+const usePut = <T>() => {
+  const [data, setData] = useState<T | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const put = async <U>(url: string, data: U) => {
+    setIsLoading(true);
+
+    try {
+      const response = await axiosInstance.put(url, data);
+      setData(response.data);
+    } catch (error: unknown) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { data, isLoading, put };
+};
+
+export default usePut;
