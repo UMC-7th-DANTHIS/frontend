e46743eb55c162d2d38eec4b0e04389961a1b713
Sowon Kim <sso626262@naver.com>
2025-03-30 22:36:46 +0900

    refactor: 등록 페이지 훅 추가 및 TS 전환

---
+++ src/hooks/registration/usePost.ts
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import axiosInstance from '../../api/axios-instance';
+
+const usePost = <T>() => {
+  const [data, setData] = useState<T | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const post = async <U>(url: string, data: U) => {
+    setIsLoading(true);
+
+    try {
+      const response = await axiosInstance.post(url, data);
+      setData(response.data);
+    } catch (error: unknown) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { data, isLoading, post };
+};
+
+export default usePost;
