ef98c32812972a14abd8c54376d08a004ee52c85
Sowon Kim <sso626262@naver.com>
2025-04-08 16:11:57 +0900

    refactor: 등록 페이지 TS 전환 (#151)

---
+++ src/pages/registration/_components/RegisterComplete.tsx
@@ -0,0 +1,101 @@
+import { useEffect } from 'react';
+import styled from 'styled-components';
+import Check from '../../../assets/check.svg';
+import { useNavigate } from 'react-router-dom';
+
+const RegisterComplete = ({
+  title,
+  detail
+}: {
+  title: string;
+  detail: string;
+}) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.scrollTo(0, 180);
+  }, []);
+
+  return (
+    <Container>
+      <Ellipse>
+        <Check />
+      </Ellipse>
+      <Title>{title}</Title>
+      <Detail>{detail}</Detail>
+      <HomeBtn
+        type="button"
+        onClick={() =>
+          navigate('/', {
+            replace: false,
+            state: {}
+          })
+        }
+      >
+        <HomeBtnText>홈 화면으로 돌아가기</HomeBtnText>
+      </HomeBtn>
+    </Container>
+  );
+};
+
+export default RegisterComplete;
+
+const Container = styled.div`
+  justify-items: center;
+  padding-bottom: 182px;
+`;
+const Ellipse = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 320px;
+  height: 320px;
+  flex-shrink: 0;
+  border-radius: 320px;
+  background: var(
+    --main-gradation,
+    linear-gradient(90deg, #b30505 0%, #9819c3 100%)
+  );
+`;
+const Title = styled.div`
+  color: var(--main_white, #fff);
+  font-family: Pretendard;
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-top: 70px;
+  margin-bottom: 40px;
+`;
+const Detail = styled.div`
+  color: var(--text_secondary-gray, #b2b2b2);
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  white-space: pre-line;
+`;
+const HomeBtn = styled.button`
+  width: 300px;
+  height: 52px;
+  flex-shrink: 0;
+  border: none;
+  border-radius: 15px;
+  background: var(--main_purple, #9819c3);
+  margin-top: 60px;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+const HomeBtnText = styled.div`
+  color: var(--main_white, #fff);
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
---
+++ src/pages/registration/_components/SubmitButton.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+const SubmitButton = ({ text }: { text: string }) => {
+  return (
+    <Btn type="submit">
+      <BtnText>{text}</BtnText>
+    </Btn>
+  );
+};
+
+export default SubmitButton;
+
+const Btn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 300px;
+  height: 52px;
+  flex-shrink: 0;
+  margin-top: 34px;
+  border: none;
+  border-radius: 15px;
+  background: var(--main_purple, #9819c3);
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+const BtnText = styled.span`
+  color: var(--main_white, #fff);
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
