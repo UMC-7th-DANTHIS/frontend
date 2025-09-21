0c6f9d4c466dbb3263e844c115e5f3c147591d2f
Sowon Kim <sso626262@naver.com>
2025-09-21 23:37:09 +0900

    chore: useGetClasses에 date 파라미터 추가

--- src/hooks/reservation/useGetClasses.ts
+++ src/hooks/reservation/useGetClasses.ts
@@ -2,10 +2,10 @@ import { GetClassesAllDto } from '../../types/reservation';
 import { fetchClasses } from '../../api/reservation';
 import { useQuery } from '@tanstack/react-query';
 
-export default function useGetClasses({ genre, page, size, day }: GetClassesAllDto) {
+export default function useGetClasses({ genre, page, size, day, date }: GetClassesAllDto) {
   return useQuery({
-    queryKey: ['classes', genre, page, size, day],
-    queryFn: () => fetchClasses({ genre, page, size, day }),
+    queryKey: ['classes', genre, page, size, day, date],
+    queryFn: () => fetchClasses({ genre, page, size, day, date }),
     staleTime: 1000 * 60 * 5, // 5분
     gcTime: 1000 * 60 * 10, // 10분
     select: (data) => data.data
