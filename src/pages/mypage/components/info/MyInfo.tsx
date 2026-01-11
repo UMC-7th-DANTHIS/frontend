1f538a0e967bc896d1d1c4421f6f784d96e882b9
chasyuss <cotn963@gmail.com>
2026-01-11 19:58:32 +0900

    fix: 로그아웃, 회원탈퇴 누르기 영역 확장

--- src/pages/mypage/components/info/MyInfo.tsx
+++ src/pages/mypage/components/info/MyInfo.tsx
@@ -72,9 +72,9 @@ const MyInfo = () => {
   return (
     <InfoContainer>
       <LeftSection>
-        <MenuItem>
+        <MenuItem onClick={handleLogoutAlert}>
           <MenuText>로그아웃</MenuText>
-          <InfoArrow onClick={handleLogoutAlert} />
+          <InfoArrow />
           {showLogoutAlert && (
             <ModalTwoBtns
               message={
@@ -89,9 +89,9 @@ const MyInfo = () => {
             />
           )}
         </MenuItem>
-        <MenuItem>
+        <MenuItem onClick={gotoQuit}>
           <MenuText>회원탈퇴</MenuText>
-          <InfoArrow onClick={gotoQuit} />
+          <InfoArrow />
         </MenuItem>
       </LeftSection>
 
