8c05b3635acce41d5f2bab62bee727d29e423dd5
mlnls <tmdals140@naver.com>
2026-01-24 16:33:22 +0900

    feat 홈에서 미로그인시 노출 페이지 변경 feat: #275

--- src/pages/Home/UserRecommend/index.tsx
+++ src/pages/Home/UserRecommend/index.tsx
@@ -3,6 +3,7 @@ import * as S from './styled';
 
 import ForDancer from '../../../common/Home/ForDancer';
 import ForClass from '../../../common/Home/ForClass';
+import { NoLogin } from '../../../common/Home/NoLogin';
 
 import { AllClassData, AllDancerData } from '@/types/MainInterface';
 import { UserContext } from '@/types/Context/User';
@@ -17,28 +18,35 @@ export const UserRecommend = ({ dancer, danceclass }: UserRecommendProps) => {
 
   return (
     <S.Container>
-      <S.Wrapper>
-        {/* <ForUserBanner /> */}
-        <S.Header>
-          {user?.nickname} 님의 스타일에 맞는 댄서를 소개할게요
-        </S.Header>
-        <S.AlignCenter>
-          <ForDancer dancer={dancer} />
-        </S.AlignCenter>
-      </S.Wrapper>
-      <S.Border />
-      <div>
+      <S.Content $isBlur={!user}>
         <S.Wrapper>
           <S.Header>
-            오로지 {user?.nickname}님을 위한 맞춤형 수업이에요
+            {user?.nickname} 님의 스타일에 맞는 댄서를 소개할게요
           </S.Header>
-        </S.Wrapper>
-        <S.ClassWrapper>
           <S.AlignCenter>
-            <ForClass danceclass={danceclass} />
+            <ForDancer dancer={dancer} />
           </S.AlignCenter>
-        </S.ClassWrapper>
-      </div>
+        </S.Wrapper>
+        <S.Border />
+        <div>
+          <S.Wrapper>
+            <S.Header>
+              오로지 {user?.nickname}님을 위한 맞춤형 수업이에요
+            </S.Header>
+          </S.Wrapper>
+          <S.ClassWrapper>
+            <S.AlignCenter>
+              <ForClass danceclass={danceclass} />
+            </S.AlignCenter>
+          </S.ClassWrapper>
+        </div>
+      </S.Content>
+
+      {!user && (
+        <S.NoLoginWrapper>
+          <NoLogin />
+        </S.NoLoginWrapper>
+      )}
     </S.Container>
   );
 };
--- src/pages/Home/UserRecommend/styled.ts
+++ src/pages/Home/UserRecommend/styled.ts
@@ -1,12 +1,30 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
+  position: relative;
   width: 100%;
   padding-bottom: 60px;
 
   ${({ theme }) => theme.media.tablet} {
     width: 100%;
   }
+`;
+
+const Content = styled.div<{ $isBlur: boolean }>`
+  width: 100%;
+  filter: ${({ $isBlur }) => ($isBlur ? 'blur(8px)' : 'none')};
+  pointer-events: ${({ $isBlur }) => ($isBlur ? 'none' : 'auto')};
+`;
+
+const NoLoginWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+
+  z-index: 10;
 `;
 
 const Wrapper = styled.div`
@@ -83,4 +101,13 @@ const Header = styled.div`
   line-height: normal;
 `;
 
-export { Container, Wrapper, ClassWrapper, AlignCenter, Border, Header };
+export {
+  Container,
+  Content,
+  NoLoginWrapper,
+  Wrapper,
+  ClassWrapper,
+  AlignCenter,
+  Border,
+  Header
+};
