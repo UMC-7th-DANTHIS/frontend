779e1e74b2a9e183975d842023292a4408b46b15
mlnls <tmdals140@naver.com>
2026-01-25 17:28:19 +0900

    feat: 신고 버튼 추가

--- src/common/Comunity/PostContent.tsx
+++ src/common/Comunity/PostContent.tsx
@@ -14,6 +14,7 @@ import axiosInstance from '../../api/axios-instance';
 
 import { SinglePostData } from '@/types/CommunityInterface';
 import { UserData } from '@/types/UserInterface';
+import { toast } from 'react-toastify';
 
 type PostContentProps = {
   length: number;
@@ -50,6 +51,10 @@ const PostContent = ({
     }
   };
 
+  const handleReport = () => {
+    toast.error('게시글이 신고되었습니다.');
+  };
+
   return (
     <>
       <PostInfo>
@@ -81,7 +86,11 @@ const PostContent = ({
           </PostActions>
         ) : (
           <PostActions>
-            <ReportButton src={Alert} alt={'Alert'} />
+            <ReportButton
+              onClick={() => handleReport()}
+              src={Alert}
+              alt={'Alert'}
+            />
           </PostActions>
         )}
         <PostMeta>
